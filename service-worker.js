d211b4286e1ce9eb14d4643cc654e52302b447e0
Roshan Yadav <ry9184721@gmail.com>
2026-09-15 17:41:48 +0545

    Refresh cached family tree script

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "roshan-pwa-v1.0.4";
+const CACHE_VERSION = "roshan-pwa-v1.0.5";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
